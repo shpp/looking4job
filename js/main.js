593ca26de479ba28f07f5fb77103a5e308930ca1
tideee2 <tideee5@yandex.ru>
2016-12-30 01:26:44 +0200

    changes: first step to pixel perfect:)

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	// $(window).resize(function(){
+	// 	var
+	// 		triangleWidth = $('body').width() / 2 + 'px solid transparent';
+	// $('.top-left, .bottom-left').css('border-right', triangleWidth);
+	// $('.top-right, .bottom-right').css('border-left', triangleWidth);
+	// });
+});
