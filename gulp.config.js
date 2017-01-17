993e77a2928da90acd8dad8f67966ae391becbe5
nsmirnova <smirnovanastya1602@gmail.com>
2017-01-17 12:23:38 +0200

    Remove unnecessary comments from gulp config

--- gulp.config.js
+++ gulp.config.js
@@ -3,11 +3,6 @@ module.exports = function () {
   var name = 'l4j';
   var build = root + 'build/';
   var styles = root + 'styles/';
-  // var sources = root + 'sources/';
-  // var assets = sources + 'assets/';
-  // var app = sources + 'app/';
-  // var lib = sources + 'lib/';
-  // var assetsStylesFolder = assets + 'styles/';
 
   var config = {
     root: root,
@@ -17,22 +12,6 @@ module.exports = function () {
     styles: styles,
     buildCss: build + name + '.css',
     buildMinCss: build + name + '.min.css'
-    // root: root,
-    // app: app,
-    // index: root + 'index.html',
-    // assetsStylesFolder: assetsStylesFolder,
-    // indexCss: app + 'index.css',
-    // sourcesStyles: app + '**/*.css',
-    // sourcesScripts: [
-    //   lib + '**/*.js',
-    //   app + '**/*.js',
-    // ],
-    // styles: assetsStylesFolder + '/*.css',
-    // scripts: [
-    //   lib + '**/*.js',
-    //   app + '**/*.js',
-    //   root + '*.js'
-    // ]
   };
 
   return config;
